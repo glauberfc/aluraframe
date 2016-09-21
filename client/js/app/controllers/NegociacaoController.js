d4bc8c275e512b680923ca514a47d6586740d69d
Glauber FC <Glauber FC>
2016-09-21 16:51:54 -0300

    Adicionando Negociações View

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,11 +7,14 @@ class NegociacaoController {
 		this._inputQuantidade = $('#quantidade');
 		this._inputValor = $('#valor');
 		this._listaNegociacoes = new ListaNegociacoes();
+		this.__negociacoesView = new NegociacoesView($('#negociacoesView'));
+		this.__negociacoesView.update(this._listaNegociacoes);
 	}
 
 	adiciona(event) {
 		event.preventDefault();
 		this._listaNegociacoes.adiciona(this._criaNegociacao());
+		this.__negociacoesView.update(this._listaNegociacoes);
 		this._limpaFormulario();
 	}
 
